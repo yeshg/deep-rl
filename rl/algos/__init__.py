f88a3f3f530188d59b39fa9986050953d185bbd4
DRL-Bender <drl@drl.com>
2019-06-19 10:13:09 -0700

    ikostrov ddpg added

--- rl/algos/__init__.py
+++ rl/algos/__init__.py
@@ -1,2 +1,3 @@
 from .ppo import PPO
+from .ddpg import DDPG
 #from .dagger import DAgger
---
+++ rl/algos/ddpg.py
@@ -0,0 +1,149 @@
+"""Deep Deterministic Policy Gradient."""
+
+# For Replay Buffer
+import random
+from collections import namedtuple
+
+
+from copy import deepcopy
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim import Adam
+from torch.autograd import Variable
+
+# From ikostrov's repo https://github.com/ikostrikov/pytorch-ddpg-naf
+def soft_update(target, source, tau):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
+
+def hard_update(target, source):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(param.data)
+
+
+Transition = namedtuple(
+'Transition', ('state', 'action', 'mask', 'next_state', 'reward'))
+
+class DDPGBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+class DDPG(object):
+    def __init__(self, gamma, tau, hidden_size, num_inputs, action_space):
+
+        self.num_inputs = num_inputs
+        self.action_space = action_space
+
+        self.actor = Actor(hidden_size, self.num_inputs, self.action_space)
+        self.actor_target = Actor(hidden_size, self.num_inputs, self.action_space)
+        self.actor_perturbed = Actor(hidden_size, self.num_inputs, self.action_space)
+        self.actor_optim = Adam(self.actor.parameters(), lr=1e-4)
+
+        self.critic = Critic(hidden_size, self.num_inputs, self.action_space)
+        self.critic_target = Critic(hidden_size, self.num_inputs, self.action_space)
+        self.critic_optim = Adam(self.critic.parameters(), lr=1e-3)
+
+        self.gamma = gamma
+        self.tau = tau
+
+        hard_update(self.actor_target, self.actor)  # Make sure target is with the same weight
+        hard_update(self.critic_target, self.critic)
+
+
+    def select_action(self, state, action_noise=None, param_noise=None):
+        self.actor.eval()
+        if param_noise is not None: 
+            mu = self.actor_perturbed((Variable(state)))
+        else:
+            mu = self.actor((Variable(state)))
+
+        self.actor.train()
+        mu = mu.data
+
+        if action_noise is not None:
+            mu += torch.Tensor(action_noise.noise())
+
+        return mu.clamp(-1, 1)
+
+
+    def update_parameters(self, batch):
+        state_batch = Variable(torch.cat(batch.state))
+        action_batch = Variable(torch.cat(batch.action))
+        reward_batch = Variable(torch.cat(batch.reward))
+        mask_batch = Variable(torch.cat(batch.mask))
+        next_state_batch = Variable(torch.cat(batch.next_state))
+        
+        next_action_batch = self.actor_target(next_state_batch)
+        next_state_action_values = self.critic_target(next_state_batch, next_action_batch)
+
+        reward_batch = reward_batch.unsqueeze(1)
+        mask_batch = mask_batch.unsqueeze(1)
+        expected_state_action_batch = reward_batch + (self.gamma * mask_batch * next_state_action_values)
+
+        self.critic_optim.zero_grad()
+
+        state_action_batch = self.critic((state_batch), (action_batch))
+
+        value_loss = F.mse_loss(state_action_batch, expected_state_action_batch)
+        value_loss.backward()
+        self.critic_optim.step()
+
+        self.actor_optim.zero_grad()
+
+        policy_loss = -self.critic((state_batch),self.actor((state_batch)))
+
+        policy_loss = policy_loss.mean()
+        policy_loss.backward()
+        self.actor_optim.step()
+
+        soft_update(self.actor_target, self.actor, self.tau)
+        soft_update(self.critic_target, self.critic, self.tau)
+
+        return value_loss.item(), policy_loss.item()
+
+    def perturb_actor_parameters(self, param_noise):
+        """Apply parameter noise to actor model, for exploration"""
+        hard_update(self.actor_perturbed, self.actor)
+        params = self.actor_perturbed.state_dict()
+        for name in params:
+            if 'ln' in name: 
+                pass 
+            param = params[name]
+            param += torch.randn(param.shape) * param_noise.current_stddev
+
+    def save_model(self, env_name, suffix="", actor_path=None, critic_path=None):
+        if not os.path.exists('models/'):
+            os.makedirs('models/')
+
+        if actor_path is None:
+            actor_path = "models/ddpg_actor_{}_{}".format(env_name, suffix) 
+        if critic_path is None:
+            critic_path = "models/ddpg_critic_{}_{}".format(env_name, suffix) 
+        print('Saving models to {} and {}'.format(actor_path, critic_path))
+        torch.save(self.actor.state_dict(), actor_path)
+        torch.save(self.critic.state_dict(), critic_path)
+
+    def load_model(self, actor_path, critic_path):
+        print('Loading models from {} and {}'.format(actor_path, critic_path))
+        if actor_path is not None:
+            self.actor.load_state_dict(torch.load(actor_path))
+        if critic_path is not None: 
+            self.critic.load_state_dict(torch.load(critic_path))
