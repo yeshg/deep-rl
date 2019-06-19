3736d1878597ac2946e1ff82b8549ed2863882d8
DRL-Bender <drl@drl.com>
2019-06-19 12:10:38 -0700

    merging with deeprl

--- examples/ddpg.py
+++ examples/ddpg.py
@@ -1,21 +1,179 @@
-
-
 import argparse
-import math
-from collections import namedtuple
-from itertools import count
-from tqdm import tqdm
-from tensorboardX import SummaryWriter
+#from tensorboardX import SummaryWriter
 
 import gym
+import gym_cassie
+
 import numpy as np
-from gym import wrappers
 
 import torch
+import torch.nn as nn
+import torch.nn.functional as F 
+import torch.autograd
+from torch.autograd import Variable
 
-from rl.utils import OUNoise, AdaptiveParamNoiseSpec, ddpg_distance_metric
-from rl.policies import Actor, Critic
-from rl.algos import DDPG
+from torch.optim import Adam
+
+# WHY ISN"T THIS WORKING?
+#from rl.utils import OUNoise, AdaptiveParamNoiseSpec, ddpg_distance_metric
+#from rl.policies import Actor, Critic
+#from rl.algos import DDPG
+
+# For Replay Buffer
+import random
+from collections import namedtuple
+
+
+# TODO: put this in utils, refactor out of the scripts in examples
+def gym_factory(path, **kwargs):
+    from functools import partial
+
+    """
+    This is (mostly) equivalent to gym.make(), but it returns an *uninstantiated* 
+    environment constructor.
+
+    Since environments containing cpointers (e.g. Mujoco envs) can't be serialized, 
+    this allows us to pass their constructors to Ray remote functions instead 
+    (since the gym registry isn't shared across ray subprocesses we can't simply 
+    pass gym.make() either)
+
+    Note: env.unwrapped.spec is never set, if that matters for some reason.
+    """
+    spec = gym.envs.registry.spec(path)
+    _kwargs = spec._kwargs.copy()
+    _kwargs.update(kwargs)
+    
+    if callable(spec._entry_point):
+        cls = spec._entry_point(**_kwargs)
+    else:
+        cls = gym.envs.registration.load(spec._entry_point)
+
+    return partial(cls, **_kwargs)
+
+
+# from https://github.com/songrotek/DDPG/blob/master/ou_noise.py
+class OUNoise:
+
+    def __init__(self, action_dimension, scale=0.1, mu=0, theta=0.15, sigma=0.2):
+        self.action_dimension = action_dimension
+        self.scale = scale
+        self.mu = mu
+        self.theta = theta
+        self.sigma = sigma
+        self.state = np.ones(self.action_dimension) * self.mu
+        self.reset()
+
+    def reset(self):
+        self.state = np.ones(self.action_dimension) * self.mu
+
+    def noise(self):
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(len(x))
+        self.state = x + dx
+        return self.state * self.scale
+
+"""
+From OpenAI Baselines:
+https://github.com/openai/baselines/blob/master/baselines/ddpg/noise.py
+"""
+class AdaptiveParamNoiseSpec(object):
+    def __init__(self, initial_stddev=0.1, desired_action_stddev=0.2, adaptation_coefficient=1.01):
+        """
+        Note that initial_stddev and current_stddev refer to std of parameter noise, 
+        but desired_action_stddev refers to (as name notes) desired std in action space
+        """
+        self.initial_stddev = initial_stddev
+        self.desired_action_stddev = desired_action_stddev
+        self.adaptation_coefficient = adaptation_coefficient
+
+        self.current_stddev = initial_stddev
+
+    def adapt(self, distance):
+        if distance > self.desired_action_stddev:
+            # Decrease stddev.
+            self.current_stddev /= self.adaptation_coefficient
+        else:
+            # Increase stddev.
+            self.current_stddev *= self.adaptation_coefficient
+
+    def get_stats(self):
+        stats = {
+            'param_noise_stddev': self.current_stddev,
+        }
+        return stats
+
+    def __repr__(self):
+        fmt = 'AdaptiveParamNoiseSpec(initial_stddev={}, desired_action_stddev={}, adaptation_coefficient={})'
+        return fmt.format(self.initial_stddev, self.desired_action_stddev, self.adaptation_coefficient)
+
+def ddpg_distance_metric(actions1, actions2):
+    """
+    Compute "distance" between actions taken by two policies at the same states
+    Expects numpy arrays
+    """
+    diff = actions1-actions2
+    mean_diff = np.mean(np.square(diff), axis=0)
+    dist = sqrt(np.mean(mean_diff))
+    return dist
+
+
+class Actor(nn.Module):
+    def __init__(self, hidden_size, num_inputs, action_space):
+        super(Actor, self).__init__()
+        self.action_space = action_space
+        num_outputs = action_space.shape[0]
+
+        self.linear1 = nn.Linear(num_inputs, hidden_size)
+        self.ln1 = nn.LayerNorm(hidden_size)
+
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.ln2 = nn.LayerNorm(hidden_size)
+
+        self.mu = nn.Linear(hidden_size, num_outputs)
+        self.mu.weight.data.mul_(0.1)
+        self.mu.bias.data.mul_(0.1)
+
+    def forward(self, inputs):
+        x = inputs
+        x = self.linear1(x)
+        x = self.ln1(x)
+        x = F.relu(x)
+        x = self.linear2(x)
+        x = self.ln2(x)
+        x = F.relu(x)
+        mu = F.tanh(self.mu(x))
+        return mu
+
+class Critic(nn.Module):
+    def __init__(self, hidden_size, num_inputs, action_space):
+        super(Critic, self).__init__()
+        self.action_space = action_space
+        num_outputs = action_space.shape[0]
+
+        self.linear1 = nn.Linear(num_inputs, hidden_size)
+        self.ln1 = nn.LayerNorm(hidden_size)
+
+        self.linear2 = nn.Linear(hidden_size+num_outputs, hidden_size)
+        self.ln2 = nn.LayerNorm(hidden_size)
+
+        self.V = nn.Linear(hidden_size, 1)
+        self.V.weight.data.mul_(0.1)
+        self.V.bias.data.mul_(0.1)
+
+    def forward(self, inputs, actions):
+        x = inputs
+        x = self.linear1(x)
+        x = self.ln1(x)
+        x = F.relu(x)
+
+        x = torch.cat((x, actions), 1)
+        x = self.linear2(x)
+        x = self.ln2(x)
+        x = F.relu(x)
+        V = self.V(x)
+        return V
+
+
 
 # from ikostrov's repo, for ddpg... shouldn't need to use this eventually
 class NormalizedActions(gym.ActionWrapper):
@@ -32,10 +190,145 @@ class NormalizedActions(gym.ActionWrapper):
         action = action * 2 - 1
         return action
 
-parser = argparse.ArgumentParser(description='PyTorch REINFORCE example')
-parser.add_argument('--algo', default='NAF',
-                    help='algorithm to use: DDPG | NAF')
-parser.add_argument('--env-name', default="HalfCheetah-v2",
+# From ikostrov's repo https://github.com/ikostrikov/pytorch-ddpg-naf
+def soft_update(target, source, tau):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
+
+def hard_update(target, source):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(param.data)
+
+
+Transition = namedtuple(
+'Transition', ('state', 'action', 'mask', 'next_state', 'reward'))
+
+class DDPGBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+class DDPG(object):
+    def __init__(self, gamma, tau, hidden_size, num_inputs, action_space):
+
+        self.num_inputs = num_inputs
+        self.action_space = action_space
+
+        self.actor = Actor(hidden_size, self.num_inputs, self.action_space)
+        self.actor_target = Actor(hidden_size, self.num_inputs, self.action_space)
+        self.actor_perturbed = Actor(hidden_size, self.num_inputs, self.action_space)
+        self.actor_optim = Adam(self.actor.parameters(), lr=args.actor_lr)
+
+        self.critic = Critic(hidden_size, self.num_inputs, self.action_space)
+        self.critic_target = Critic(hidden_size, self.num_inputs, self.action_space)
+        self.critic_optim = Adam(self.critic.parameters(), lr=args.critic_lr)
+
+        self.gamma = gamma
+        self.tau = tau
+
+        hard_update(self.algoarget, self.actor)  # Make sure target is with the same weight
+        hard_update(self.algotarget, self.critic)
+
+
+    def select_action(selalgoe, action_noise=None, param_noise=None):
+        self.actor.eval()
+        if param_noise is not None: 
+            # add parameter noise for exploration
+            mu = self.actor_perturbed((Variable(state)))
+        else:
+            mu = self.actor((Variable(state)))
+
+        self.actor.train()
+        mu = mu.data
+
+        if action_noise is not None:
+            # add action noise for exploration
+            mu += torch.Tensor(action_noise.noise())
+
+        return mu.clamp(-1, 1)
+
+
+    def update_parameters(self, batch):
+        state_batch = Variable(torch.cat(batch.state))
+        action_batch = Variable(torch.cat(batch.action))
+        reward_batch = Variable(torch.cat(batch.reward))
+        mask_batch = Variable(torch.cat(batch.mask))
+        next_state_batch = Variable(torch.cat(batch.next_state))
+        
+        next_action_batch = self.actor_target(next_state_batch)
+        next_state_action_values = self.critic_target(next_state_batch, next_action_batch)
+
+        reward_batch = reward_batch.unsqueeze(1)
+        mask_batch = mask_batch.unsqueeze(1)
+        expected_state_action_batch = reward_batch + (self.gamma * mask_batch * next_state_action_values)
+
+        self.critic_optim.zero_grad()
+
+        state_action_batch = self.critic((state_batch), (action_batch))
+
+        value_loss = F.mse_loss(state_action_batch, expected_state_action_batch)
+        value_loss.backward()
+        self.critic_optim.step()
+
+        self.actor_optim.zero_grad()
+
+        policy_loss = -self.critic((state_batch),self.actor((state_batch)))
+
+        policy_loss = policy_loss.mean()
+        policy_loss.backward()
+        self.actor_optim.step()
+
+        soft_update(self.actor_target, self.actor, self.tau)
+        soft_update(self.critic_target, self.critic, self.tau)
+
+        return value_loss.item(), policy_loss.item()
+
+    def perturb_actor_parameters(self, param_noise):
+        """Apply parameter noise to actor model, for exploration"""
+        hard_update(self.actor_perturbed, self.actor)
+        params = self.actor_perturbed.state_dict()
+        for name in params:
+            if 'ln' in name: 
+                pass 
+            param = params[name]
+            param += torch.randn(param.shape) * param_noise.current_stddev
+
+    def save_model(self, env_name, suffix="", actor_path=None, critic_path=None):
+        if not os.path.exists('models/'):
+            os.makedirs('models/')
+
+        if actor_path is None:
+            actor_path = "models/ddpg_actor_{}_{}".format(env_name, suffix) 
+        if critic_path is None:
+            critic_path = "models/ddpg_critic_{}_{}".format(env_name, suffix) 
+        print('Saving models to {} and {}'.format(actor_path, critic_path))
+        torch.save(self.actor.state_dict(), actor_path)
+        torch.save(self.critic.state_dict(), critic_path)
+
+    def load_model(self, actor_path, critic_path):
+        print('Loading models from {} and {}'.format(actor_path, critic_path))
+        if actor_path is not None:
+            self.actor.load_state_dict(torch.load(actor_path))
+        if critic_path is not None: 
+            self.critic.load_state_dict(torch.load(critic_path))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--env-name', type=str, default="HalfCheetah-v2",
                     help='name of the environment to run')
 parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
                     help='discount factor for reward (default: 0.99)')
@@ -51,8 +344,6 @@ parser.add_argument('--exploration_end', type=int, default=100, metavar='N',
                     help='number of episodes with noise (default: 100)')
 parser.add_argument('--seed', type=int, default=4, metavar='N',
                     help='random seed (default: 4)')
-parser.add_argument('--batch_size', type=int, default=128, metavar='N',
-                    help='batch size (default: 128)')
 parser.add_argument('--num_steps', type=int, default=1000, metavar='N',
                     help='max episode length (default: 1000)')
 parser.add_argument('--num_episodes', type=int, default=1000, metavar='N',
@@ -65,21 +356,26 @@ parser.add_argument('--replay_size', type=int, default=1000000, metavar='N',
                     help='size of replay buffer (default: 1000000)')
 args = parser.parse_args()
 
+args.batch_size = 128
+args.actor_lr = 1e-3
+args.critic_lr = 1e-4
+
+#args.num_procs = 30   # this will be used later for D4PG
+
 env = NormalizedActions(gym.make(args.env_name))
+
+#env_fn = gym_factory(args.env-name)
+
 
 writer = SummaryWriter()
 
 env.seed(args.seed)
 torch.manual_seed(args.seed)
 np.random.seed(args.seed)
-if args.algo == "NAF":
-    agent = NAF(args.gamma, args.tau, args.hidden_size,
-                      env.observation_space.shape[0], env.action_space)
-else:
-    agent = DDPG(args.gamma, args.tau, args.hidden_size,
-                      env.observation_space.shape[0], env.action_space)
+agent = DDPG(args.gamma, args.tau, args.hidden_size,
+                    env.observation_space.shape[0], env.action_space)
 
-memory = ReplayMemory(args.replay_size)
+memory = DDPGBuffer(args.replay_size)
 
 ounoise = OUNoise(env.action_space.shape[0]) if args.ou_noise else None
 param_noise = AdaptiveParamNoiseSpec(initial_stddev=0.05, 
@@ -92,12 +388,14 @@ updates = 0
 for i_episode in range(args.num_episodes):
     state = torch.Tensor([env.reset()])
 
+    """Generate noise correlated with previous noise via Ornstein-Uhlenbeck Process"""
     if args.ou_noise: 
         ounoise.scale = (args.noise_scale - args.final_noise_scale) * max(0, args.exploration_end -
                                                                       i_episode) / args.exploration_end + args.final_noise_scale
         ounoise.reset()
 
-    if args.param_noise and args.algo == "DDPG":
+    if args.param_noise:
+        """Apply parameter noise to actor model, for exploration"""
         agent.perturb_actor_parameters(param_noise)
 
     episode_reward = 0
--- rl/algos/ddpg.py
+++ rl/algos/ddpg.py
@@ -1,17 +1,167 @@
-"""Deep Deterministic Policy Gradient."""
+import argparse
+import math
+from collections import namedtuple
+from itertools import count
+from tqdm import tqdm
+from tensorboardX import SummaryWriter
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F 
+import torch.autograd
+from torch.autograd import Variable
+
+from torch.optim import Adam
+
+# WHY ISN"T THIS WORKING?
+#from rl.utils import OUNoise, AdaptiveParamNoiseSpec, ddpg_distance_metric
+#from rl.policies import Actor, Critic
+#from rl.algos import DDPG
 
 # For Replay Buffer
 import random
 from collections import namedtuple
 
+# from https://github.com/songrotek/DDPG/blob/master/ou_noise.py
+class OUNoise:
 
-from copy import deepcopy
+    def __init__(self, action_dimension, scale=0.1, mu=0, theta=0.15, sigma=0.2):
+        self.action_dimension = action_dimension
+        self.scale = scale
+        self.mu = mu
+        self.theta = theta
+        self.sigma = sigma
+        self.state = np.ones(self.action_dimension) * self.mu
+        self.reset()
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.optim import Adam
-from torch.autograd import Variable
+    def reset(self):
+        self.state = np.ones(self.action_dimension) * self.mu
+
+    def noise(self):
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(len(x))
+        self.state = x + dx
+        return self.state * self.scale
+
+"""
+From OpenAI Baselines:
+https://github.com/openai/baselines/blob/master/baselines/ddpg/noise.py
+"""
+class AdaptiveParamNoiseSpec(object):
+    def __init__(self, initial_stddev=0.1, desired_action_stddev=0.2, adaptation_coefficient=1.01):
+        """
+        Note that initial_stddev and current_stddev refer to std of parameter noise, 
+        but desired_action_stddev refers to (as name notes) desired std in action space
+        """
+        self.initial_stddev = initial_stddev
+        self.desired_action_stddev = desired_action_stddev
+        self.adaptation_coefficient = adaptation_coefficient
+
+        self.current_stddev = initial_stddev
+
+    def adapt(self, distance):
+        if distance > self.desired_action_stddev:
+            # Decrease stddev.
+            self.current_stddev /= self.adaptation_coefficient
+        else:
+            # Increase stddev.
+            self.current_stddev *= self.adaptation_coefficient
+
+    def get_stats(self):
+        stats = {
+            'param_noise_stddev': self.current_stddev,
+        }
+        return stats
+
+    def __repr__(self):
+        fmt = 'AdaptiveParamNoiseSpec(initial_stddev={}, desired_action_stddev={}, adaptation_coefficient={})'
+        return fmt.format(self.initial_stddev, self.desired_action_stddev, self.adaptation_coefficient)
+
+def ddpg_distance_metric(actions1, actions2):
+    """
+    Compute "distance" between actions taken by two policies at the same states
+    Expects numpy arrays
+    """
+    diff = actions1-actions2
+    mean_diff = np.mean(np.square(diff), axis=0)
+    dist = sqrt(np.mean(mean_diff))
+    return dist
+
+
+class Actor(nn.Module):
+    def __init__(self, hidden_size, num_inputs, action_space):
+        super(Actor, self).__init__()
+        self.action_space = action_space
+        num_outputs = action_space.shape[0]
+
+        self.linear1 = nn.Linear(num_inputs, hidden_size)
+        self.ln1 = nn.LayerNorm(hidden_size)
+
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.ln2 = nn.LayerNorm(hidden_size)
+
+        self.mu = nn.Linear(hidden_size, num_outputs)
+        self.mu.weight.data.mul_(0.1)
+        self.mu.bias.data.mul_(0.1)
+
+    def forward(self, inputs):
+        x = inputs
+        x = self.linear1(x)
+        x = self.ln1(x)
+        x = F.relu(x)
+        x = self.linear2(x)
+        x = self.ln2(x)
+        x = F.relu(x)
+        mu = F.tanh(self.mu(x))
+        return mu
+
+class Critic(nn.Module):
+    def __init__(self, hidden_size, num_inputs, action_space):
+        super(Critic, self).__init__()
+        self.action_space = action_space
+        num_outputs = action_space.shape[0]
+
+        self.linear1 = nn.Linear(num_inputs, hidden_size)
+        self.ln1 = nn.LayerNorm(hidden_size)
+
+        self.linear2 = nn.Linear(hidden_size+num_outputs, hidden_size)
+        self.ln2 = nn.LayerNorm(hidden_size)
+
+        self.V = nn.Linear(hidden_size, 1)
+        self.V.weight.data.mul_(0.1)
+        self.V.bias.data.mul_(0.1)
+
+    def forward(self, inputs, actions):
+        x = inputs
+        x = self.linear1(x)
+        x = self.ln1(x)
+        x = F.relu(x)
+
+        x = torch.cat((x, actions), 1)
+        x = self.linear2(x)
+        x = self.ln2(x)
+        x = F.relu(x)
+        V = self.V(x)
+        return V
+
+
+
+# from ikostrov's repo, for ddpg... shouldn't need to use this eventually
+class NormalizedActions(gym.ActionWrapper):
+
+    def _action(self, action):
+        action = (action + 1) / 2  # [-1, 1] => [0, 1]
+        action *= (self.action_space.high - self.action_space.low)
+        action += self.action_space.low
+        return action
+
+    def _reverse_action(self, action):
+        action -= self.action_space.low
+        action /= (self.action_space.high - self.action_space.low)
+        action = action * 2 - 1
+        return action
 
 # From ikostrov's repo https://github.com/ikostrikov/pytorch-ddpg-naf
 def soft_update(target, source, tau):
@@ -55,22 +205,23 @@ class DDPG(object):
         self.actor = Actor(hidden_size, self.num_inputs, self.action_space)
         self.actor_target = Actor(hidden_size, self.num_inputs, self.action_space)
         self.actor_perturbed = Actor(hidden_size, self.num_inputs, self.action_space)
-        self.actor_optim = Adam(self.actor.parameters(), lr=1e-4)
+        self.actor_optim = Adam(self.actor.parameters(), lr=args.actor_lr)
 
         self.critic = Critic(hidden_size, self.num_inputs, self.action_space)
         self.critic_target = Critic(hidden_size, self.num_inputs, self.action_space)
-        self.critic_optim = Adam(self.critic.parameters(), lr=1e-3)
+        self.critic_optim = Adam(self.critic.parameters(), lr=args.critic_lr)
 
         self.gamma = gamma
         self.tau = tau
 
-        hard_update(self.actor_target, self.actor)  # Make sure target is with the same weight
-        hard_update(self.critic_target, self.critic)
+        hard_update(self.algoarget, self.actor)  # Make sure target is with the same weight
+        hard_update(self.algotarget, self.critic)
 
 
-    def select_action(self, state, action_noise=None, param_noise=None):
-        self.actor.eval()
+    def select_action(selalgoe, action_noise=None, param_noise=None):
+        self.actor.eval()algo
         if param_noise is not None: 
+            # add parameter noise for exploration
             mu = self.actor_perturbed((Variable(state)))
         else:
             mu = self.actor((Variable(state)))
@@ -79,6 +230,7 @@ class DDPG(object):
         mu = mu.data
 
         if action_noise is not None:
+            # add action noise for exploration
             mu += torch.Tensor(action_noise.noise())
 
         return mu.clamp(-1, 1)
@@ -147,3 +299,143 @@ class DDPG(object):
             self.actor.load_state_dict(torch.load(actor_path))
         if critic_path is not None: 
             self.critic.load_state_dict(torch.load(critic_path))
+
+    def train(self, env_fn, args):
+        # tbd
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--env-name', type=str, default="Cassie-mimic-walking-v0",
+                    help='name of the environment to run')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
+                    help='discount factor for reward (default: 0.99)')
+parser.add_argument('--tau', type=float, default=0.001, metavar='G',
+                    help='discount factor for model (default: 0.001)')
+parser.add_argument('--ou_noise', type=bool, default=True)
+parser.add_argument('--param_noise', type=bool, default=False)
+parser.add_argument('--noise_scale', type=float, default=0.3, metavar='G',
+                    help='initial noise scale (default: 0.3)')
+parser.add_argument('--final_noise_scale', type=float, default=0.3, metavar='G',
+                    help='final noise scale (default: 0.3)')
+parser.add_argument('--exploration_end', type=int, default=100, metavar='N',
+                    help='number of episodes with noise (default: 100)')
+parser.add_argument('--seed', type=int, default=4, metavar='N',
+                    help='random seed (default: 4)')
+parser.add_argument('--num_steps', type=int, default=1000, metavar='N',
+                    help='max episode length (default: 1000)')
+parser.add_argument('--num_episodes', type=int, default=1000, metavar='N',
+                    help='number of episodes (default: 1000)')
+parser.add_argument('--hidden_size', type=int, default=128, metavar='N',
+                    help='number of episodes (default: 128)')
+parser.add_argument('--updates_per_step', type=int, default=5, metavar='N',
+                    help='model updates per simulator step (default: 5)')
+parser.add_argument('--replay_size', type=int, default=1000000, metavar='N',
+                    help='size of replay buffer (default: 1000000)')
+args = parser.parse_args()
+
+args.batch_size = 128
+args.actor_lr = 1e-3
+args.critic_lr = 1e-4
+
+#args.num_procs = 30   # this will be used later for D4PG
+
+env = NormalizedActions(gym.make(args.env_name))
+
+env_fn = gym_factory(args.env-name)
+
+
+writer = SummaryWriter()
+
+env.seed(args.seed)
+torch.manual_seed(args.seed)
+np.random.seed(args.seed)
+agent = DDPG(args.gamma, args.tau, args.hidden_size,
+                    env.observation_space.shape[0], env.action_space)
+
+memory = DDPGBuffer(args.replay_size)
+
+ounoise = OUNoise(env.action_space.shape[0]) if args.ou_noise else None
+param_noise = AdaptiveParamNoiseSpec(initial_stddev=0.05, 
+    desired_action_stddev=args.noise_scale, adaptation_coefficient=1.05) if args.param_noise else None
+
+rewards = []
+total_numsteps = 0
+updates = 0
+
+for i_episode in range(args.num_episodes):
+    state = torch.Tensor([env.reset()])
+
+    """Generate noise correlated with previous noise via Ornstein-Uhlenbeck Process"""
+    if args.ou_noise: 
+        ounoise.scale = (args.noise_scale - args.final_noise_scale) * max(0, args.exploration_end -
+                                                                      i_episode) / args.exploration_end + args.final_noise_scale
+        ounoise.reset()
+
+    if args.param_noise:
+        """Apply parameter noise to actor model, for exploration"""
+        agent.perturb_actor_parameters(param_noise)
+
+    episode_reward = 0
+    while True:
+        action = agent.select_action(state, ounoise, param_noise)
+        next_state, reward, done, _ = env.step(action.numpy()[0])
+        total_numsteps += 1
+        episode_reward += reward
+
+        action = torch.Tensor(action)
+        mask = torch.Tensor([not done])
+        next_state = torch.Tensor([next_state])
+        reward = torch.Tensor([reward])
+
+        memory.push(state, action, mask, next_state, reward)
+
+        state = next_state
+
+        if len(memory) > args.batch_size:
+            for _ in range(args.updates_per_step):
+                transitions = memory.sample(args.batch_size)
+                batch = Transition(*zip(*transitions))
+
+                value_loss, policy_loss = agent.update_parameters(batch)
+
+                writer.add_scalar('loss/value', value_loss, updates)
+                writer.add_scalar('loss/policy', policy_loss, updates)
+
+                updates += 1
+        if done:
+            break
+
+    writer.add_scalar('reward/train', episode_reward, i_episode)
+
+    # Update param_noise based on distance metric
+    if args.param_noise:
+        episode_transitions = memory.memory[memory.position-t:memory.position]
+        states = torch.cat([transition[0] for transition in episode_transitions], 0)
+        unperturbed_actions = agent.select_action(states, None, None)
+        perturbed_actions = torch.cat([transition[1] for transition in episode_transitions], 0)
+
+        ddpg_dist = ddpg_distance_metric(perturbed_actions.numpy(), unperturbed_actions.numpy())
+        param_noise.adapt(ddpg_dist)
+
+    rewards.append(episode_reward)
+    if i_episode % 10 == 0:
+        state = torch.Tensor([env.reset()])
+        episode_reward = 0
+        while True:
+            action = agent.select_action(state)
+
+            next_state, reward, done, _ = env.step(action.numpy()[0])
+            episode_reward += reward
+
+            next_state = torch.Tensor([next_state])
+
+            state = next_state
+            if done:
+                break
+
+        writer.add_scalar('reward/test', episode_reward, i_episode)
+
+        rewards.append(episode_reward)
+        print("Episode: {}, total numsteps: {}, reward: {}, average reward: {}".format(i_episode, total_numsteps, rewards[-1], np.mean(rewards[-10:])))
+    
+env.close()
